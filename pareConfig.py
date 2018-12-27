ec0ce93b0608cc6bb2641d96ca780f06ea219f01
mustafa yavuz <paredicma@gmail.com>
2018-12-27 10:08:08 +0300

    Add files via upload

---
+++ pareConfig.py
@@ -0,0 +1,38 @@
+from pareNodeList import *
+##################PARAMETERS################################
+projectName='testProject'
+redisDataDir = '/home/hiccup/'+projectName+'/'
+redisConfigDir = '/home/hiccup/'+projectName+'/'
+redisLogDir = '/home/hiccup/'+projectName+'/'
+redisVersion = '5.0.3'
+redisTarFile = 'redis-5.0.3.tar.gz'
+redisBinaryDir = '/home/hiccup/reBin/redis-'+redisVersion+'/'
+dedicateCore = True   ## True/False
+doCompile = True
+doStartNodes = True
+unixSocketDir = '/tmp/'
+pidFileDir = '/var/run/'
+writePareLogFile = True
+pareLogFile = 'paredicma.log'
+pareTmpDir = 'temparedicma/'
+pareServerIp = '10.0.0.15' ## The server Which runs paredicma-cli.py 
+pareOSUser = 'hiccup' ##'hiccup'
+rdb = 'on'   ## on/off
+rdbValue = 'save 3600 1000\nsave 1800 10000\nsave 600 100000'
+aof = 'on'   ## on/off
+aofValue = 'appendfsync everysec'
+redisCluster = 'on'   ## on/off
+#redisClusterRepNum = '1'  ## number of replica for each master
+clusterNodeTimeout = 'cluster-node-timeout 5000'
+clusterParameters = 'cluster-replica-validity-factor 0\ncluster-migration-barrier 1'
+maxMemory = 'on'   ## on/off
+redisPwdAuthentication =  True   ## True/False
+redisPwd = 'my1Laydy7darbanville5*'
+redisParameters = '''
+daemonize yes
+slowlog-log-slower-than 1000
+latency-monitor-threshold 100
+slowlog-max-len 10
+rename-command FLUSHALL "Ahsa5sMdbuva7_avsvs*1**"
+rename-command FLUSHDB "Ahsa5sMdbuva7_avsvs*1"
+'''
---
+++ pareNodeList.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+##################NODES################################
+###pareNodes=[['node_ip'],['node_port'],['node_cpucore_id(s):semicoma-separate'],['max-memory'],[isActive]]
+pareNodes = []
+pareNodes.append([['10.0.0.15'],['7771'],['3,4'],['3gb'],True])
+pareNodes.append([['10.0.0.15'],['7772'],['5'],['3gb'],True])
+pareNodes.append([['10.0.0.15'],['7773'],['6'],['3gb'],True])
+pareNodes.append([['10.0.0.15'],['7774'],['7,8'],['3gb'],True])
+pareNodes.append([['10.0.0.16'],['7771'],['3'],['3gb'],True])
+pareNodes.append([['10.0.0.16'],['7772'],['4'],['3gb'],True])
+pareNodes.append([['10.0.0.16'],['7773'],['5,6'],['3gb'],True])
+pareNodes.append([['10.0.0.16'],['7774'],['7'],['3gb'],True])
